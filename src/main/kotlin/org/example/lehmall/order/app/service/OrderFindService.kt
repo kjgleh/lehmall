abca1635a1a1edf631d5e89cde031da70311a162
junggeun-dev <gogoogjg@tripstore.kr>
2025-09-28 17:47:34 +0900

    wip

--- src/main/kotlin/org/example/lehmall/order/app/service/OrderFindService.kt
+++ src/main/kotlin/org/example/lehmall/order/app/service/OrderFindService.kt
@@ -1,9 +1,9 @@
 package org.example.lehmall.order.app.service
 
+import org.example.lehmall.common.RepositoryExtensions.findByIdOrThrow
 import org.example.lehmall.order.app.repository.OrderRepository
 import org.example.lehmall.order.app.service.provided.OrderFinder
 import org.example.lehmall.order.app.service.provided.dto.OrderFindResponse
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,8 +14,7 @@ class OrderFindService(
 ) : OrderFinder {
 
     override fun find(id: Long): OrderFindResponse {
-        val order = orderRepository.findByIdOrNull(id)
-            ?: error("Order with id $id not found")
+        val order = orderRepository.findByIdOrThrow(id)
 
         return OrderFindResponse.of(order)
     }
